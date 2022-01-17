15eaa65372818f0cda45cc64e4d18034c5de0792
JoniMujica <jonimujica3@hotmail.com>
2022-01-17 14:30:41 -0300

    Agrego acceso a la db + ConeccionString y obtengo los datos por DataReader

---
+++ LogicaNegocio/AccesoDatos.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace LogicaNegocio
+{
+    public class AccesoDatos
+    {
+        public List<Cliente> leerClientes()
+        {
+            List<Cliente> clientes = new List<Cliente>();
+            SqlConnection connection = new SqlConnection("Server=JONII;Database=proem;Trusted_Connection=True;");
+
+            try
+            {
+                connection.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = connection;
+                cmd.CommandType = System.Data.CommandType.Text; //indica que le vamos a pasar la Query como texto
+                cmd.CommandText = "select * from Clientes"; //Ejecuta la consulta
+                SqlDataReader reader = cmd.ExecuteReader(); //ejecuta la consulta y lo formatea en SqlDataReader
+
+                while (reader.Read()) //VERIFICA SI HAY FILAS POR LEER
+                {
+                    clientes.Add(new Cliente(int.Parse(reader["Id"].ToString()),
+                        reader["Nombre"].ToString(),
+                        reader["Apellido"].ToString(), 
+                        int.Parse(reader["Telefono"].ToString()), 
+                        reader["Direccion"].ToString()));
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return clientes;
+
+        }
+    }
+}
